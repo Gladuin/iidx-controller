9e7b9c7f63d07ba09ee5e90f884f71d17ac4d505
Gladuin <gladuin@example.com>
2020-12-05 11:40:53 +0100

    fix indentation

--- iidx-controller.ino
+++ iidx-controller.ino
@@ -3,17 +3,17 @@ IIDXHID_ IIDXHID;
 
 // Pins LEDs are connected to.
 uint8_t leds[NUMBER_OF_LEDS] = {
-  2,    // button 1 led
-  4,    // button 2 led
-  6,    // button 3 led
-  8,    // button 4 led
-  10,   // button 5 led
-  12,   // button 6 led
-  18,   // button 7 led
-  20,   // misc button 1 led
-  22,   // misc button 2 led
-  14,   // misc button 3 led
-  16    // misc button 4 led
+    2,    // button 1 led
+    4,    // button 2 led
+    6,    // button 3 led
+    8,    // button 4 led
+    10,   // button 5 led
+    12,   // button 6 led
+    18,   // button 7 led
+    20,   // misc button 1 led
+    22,   // misc button 2 led
+    14,   // misc button 3 led
+    16    // misc button 4 led
 };
 
 void setup() {
