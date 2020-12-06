0a986eca2f35e7336c3446b5607edc6b22370e05
Gladuin <gladuin@example.com>
2020-12-06 22:47:51 +0100

    forgot to remove some comment

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # iidx-controller
 Software that enables you to create your own Arduino based beatmania IIDX controller.
+
+# Features
+ - HID lighting support (that works with btools and ttools)
+ - Customisable sensitivity over HID
--- iidx-controller/IIDXHID.cpp
+++ iidx-controller/IIDXHID.cpp
@@ -67,7 +67,6 @@ static const uint8_t PROGMEM hid_report[] = {
     0x19, 0x00,                      //  USAGE_MINIMUM (Instance Unused)
     0x29, 0x00,                      //  USAGE_MAXIMUM (Instance Unused)
     0x15, 0x00,                      //  LOGICAL_MINIMUM (0)
-    //0x26, 0xff, 0x00,                //  LOGICAL_MAXIMUM (255)
     0x25, 0x09,                      //  LOGICAL_MAXIMUM (9)
     0x75, 0x08,                      //  REPORT_SIZE (8)
     0x95, 0x01,                      //  REPORT_COUNT (1)
