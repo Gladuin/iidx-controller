5b29c38f06ca31522ccaf1b192db6db1e0d2dbf3
Gladuin <gladuin@example.com>
2021-12-09 22:30:55 +0100

    tidy non-destructive code style (IIDXHID.h)

--- iidx-controller/IIDXHID.h
+++ iidx-controller/IIDXHID.h
@@ -4,10 +4,9 @@
 #define NUMBER_OF_BUTTONS 11
 #define ENCODER_PPR 600
 
+#define INCREMENTS_PER_FULL_TURN 72   // 72 for 60Hz IIDX, 144 for 120Hz IIDX, 255 for 1 to 1
 
-#define INCREMENTS_PER_FULL_TURN 72   //72 for 60Hz IIDX, 144 for 120Hz IIDX, 255 for 1 to 1
-
-#define ADJUSTED_PPR ((int)((float)ENCODER_PPR * ((float)255/(float)INCREMENTS_PER_FULL_TURN)))
+#define ADJUSTED_PPR ((int)((float)ENCODER_PPR * ((float)255 / (float)INCREMENTS_PER_FULL_TURN)))
 
 #define NO_SENSITIVITY 0
 
