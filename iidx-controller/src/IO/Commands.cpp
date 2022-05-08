3dbe4646e1ca93e62aba31862d8152b5969703c1
unknown <34450360+Gladuin@users.noreply.github.com>
2022-05-08 14:25:16 +0200

    change reset command
    make sure that the hid reset command actually kicks the chip to the bootloader instead of just soft resetting

--- iidx-controller/src/IO/Commands.cpp
+++ iidx-controller/src/IO/Commands.cpp
@@ -3,9 +3,19 @@
 
 #include "Commands.h"
 
+void reboot() {
+    // https://blog.fsck.com/2014/08/how-to-reboot-an-arduino-leonardo-micro-into-the-bootloader.html
+    uint16_t boot_key = 0x7777;
+    uint16_t *const boot_key_ptr = (uint16_t *)0x0800;
+    
+    *boot_key_ptr = boot_key;
+    
+    wdt_enable(WDTO_15MS);
+    while (true) {}
+}
+
 void process_command(uint16_t data) {
     if ((data & 0xFF) == 0xFF) {
-        wdt_enable(WDTO_15MS);
-        while (true) {}
+        reboot();
     }
 }
