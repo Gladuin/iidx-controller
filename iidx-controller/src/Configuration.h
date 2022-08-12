3117b4fe8d6ffd452b0c46b24df22c79cc713af8
Gladuin <34450360+Gladuin@users.noreply.github.com>
2022-08-12 22:36:37 +0200

    append for previous commit

--- iidx-controller/src/Configuration.h
+++ iidx-controller/src/Configuration.h
@@ -8,7 +8,7 @@
     typedef struct {
         uint8_t controller_mode;            // 0 = joystick, 1 = keyboard
         uint8_t led_mode;                   // 0 = reactive + hid, 1 = hid, 2 = reactive, 3 = off
-        uint8_t tt_mode;                    // 0 = analog, 1 = digital
+        uint8_t tt_mode;                    // 0 = analog joystick, 1 = analog mouse, 2 = digital joystick, 3 = digital button, 4 = digital keyboard
         uint8_t increments_per_full_turn;   // 72 for 60Hz IIDX, 144 for 120Hz IIDX, 255 for 1 to 1
         uint8_t debounce_time;              // 1 - 255, in ms
         uint8_t polling_rate;               // 1 - 255, in ms
